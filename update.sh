cf6ed2a224e468b21741dd774d4c1f3b947ca9d4
Joe <73498493+jserra99@users.noreply.github.com>
2022-01-02 01:18:35 -0800

    Update and rename update.sh to start.sh

--- update.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-sudo apt-get update
-sudo apt-get upgrade
-rm chameleon-vision.jar
-curl -s https://api.github.com/repos/chameleon-vision/chameleon-vision/releases/latest | grep "browser_download_url.*jar" | cut -d : -f 2,3 | tr -d '"' | wget -qi - -O chameleon-vision.jar
-
-echo "Chameleon Vision is ready for use! run \"sudo java -jar chameleon-vision.jar\" to start!"
