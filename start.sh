bbd04a79ec08d65a970f01b4e052c4045dde7283
Joe <73498493+jserra99@users.noreply.github.com>
2022-01-02 02:01:53 -0800

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 echo "Please enter the new password to use to login to the pi, as a member of team 753 you should know what to put"
 sudo passwd pi
+sudo apt-get update
+sudo apt-get upgrade
 git clone https://github.com/jserra99/aether-framework.git
 sudo pip3 install adafruit-circuitpython-neopixel
