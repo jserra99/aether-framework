e257b68f7a6e02f283f1d04c4169db7368969e09
Joe <73498493+jserra99@users.noreply.github.com>
2022-02-24 15:32:07 -0800

    made necessary changes

--- neopixel-framework.py
+++ neopixel-framework.py
@@ -5,59 +5,38 @@ import time
 import board
 import neopixel
 import RPi.GPIO as GPIO
+import logging
+
+logging.basicConfig(level=logging.DEBUG)
 
 GPIO.setmode(GPIO.BCM)
 num_pixels = 3
 ORDER = neopixel.RGBW
 pixel_pin = board.D18 # Neo pixel digital out connected to the 6th pin down on the second column
-brightness_ = 1.0
+brightness_ = 0.1
 pixels = neopixel.NeoPixel(pixel_pin, num_pixels, brightness=brightness_, auto_write=False, pixel_order=ORDER)
-
-DEBUG = True # change this to False to get rid of processor-wasteful print statements
-
-def debug(message):
-    if DEBUG:
-        print(message)
-
+pixels.fill((255, 0, 0, 0))
+pixels.show()
 cond = threading.Condition()
-notified = [False]
-connected = False
-def connectionListener(connected, info):
-    print(info, '; Connected=%s' % connected)
-    with cond:
-        notified[0] = True
-        connected = True
-        pixels.fill((0, 0, 0, 0))
-        pixels.show()
-        cond.notify()
-
-NetworkTables.initialize(server='10.7.53.2') # roborio-753-frc.local
-NetworkTables.addConnectionListener(connectionListener, immediateNotify=True)
-
-with cond:
-    print('Waiting')
-    pixels.fill((255, 0, 0, 0))
-    pixels.show()
-    if not notified[0]:
-        cond.wait()
+NetworkTables.initialize(server='192.168.1.20') # roborio-753-frc.local
+sd = NetworkTables.getTable("SmartDashboard")
 
 # Our main code goes here
 
 def main():
-    table = NetworkTables.getTable('SmartDashboard')
-    limelight = table.getAutoUpdateValue('limelight', False) # First parameter is the name of the instance, the second is the default value if nothing is found.
-    robotEnabled = table.getAutoUpdateValue('robotEnabledPlaceholder', False)
+    limelight = sd.getAutoUpdateValue('aether', False) # First parameter is the name of the instance, the second is the default value if nothing is found.
+    robotEnabled = sd.getAutoUpdateValue('robotEnabled', False)
     startTime = time.perf_counter()
     while True:
         timeElapsed = time.perf_counter() - startTime
-        pixels.brightness = 0.05
-        if robotEnabled:
-            if limelight:
+        if robotEnabled.value:
+            if limelight.value:
                 pixels.brightness = 1.0
                 pixels.fill((0, 255, 0, 0))
             else:
                 pixels.fill((0, 0, 0, 0))
         else:
+            pixels.brightness = 0.05
             if (timeElapsed % 2) >= 1:
                 pixels.fill((0, 0, 255, 0))
             else:
@@ -65,10 +44,11 @@ def main():
         pixels.show()
         time.sleep(0.02)
 
+
 if __name__ == '__main__':
     try:
         main()
     except:
-        print("exception: exiting program")
+        print("exception; exiting program")
         pixels.fill((0, 0, 0, 0))
         GPIO.cleanup()
